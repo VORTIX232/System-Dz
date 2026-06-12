52afa0bc965aba71155e67f96a441f79e62c5468
baby bobmarly strawbry elphant <thorfoxy83@gmail.com>
2026-06-12 18:38:25 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "بوت سيستم لي الجزائر",
+        name: "🫡بوت مخصص لي سيرفر الجزائر",
         // Activity type number (0 = Playing).
         type: 0, 
       },
